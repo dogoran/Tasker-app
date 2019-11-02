578e0daac34692939aca4dc9045466a1d5dd1e57
Andrii Mashevskyi <dogoran@mail.ru>
2019-11-02 13:22:06 +0200

    Add cookie utils

---
+++ src/utils/cookie.js
@@ -0,0 +1,16 @@
+export function getCookie(name) {
+  // eslint-disable-next-line no-useless-escape
+  const matches = document.cookie.match(new RegExp(`(?:^|; )${name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1')}=([^;]*)`));
+
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+export function setCookie(cookies, maxAge = '') {
+  Object.keys(cookies).forEach((key) => {
+    document.cookie = `${key}=${cookies[key]}${maxAge && `;max-age=${maxAge}`};path=/`;
+  });
+}
+
+export function isTokenValid() {
+  return getCookie('token');
+}
